6727ff303bbcc3f42187cf05e5e2f27bfe77d84e
Daniel Beck <daniel.e.beck@gmail.com>
2017-01-02 19:14:55 +1100

    added bias'

--- emotions/src/run_avg_ind_none.sh
+++ emotions/src/run_avg_ind_none.sh
@@ -7,6 +7,13 @@ OMP_NUM_THREADS=1 python avg_model_ind.py ../../data/instances.txt ../../data/em
 OMP_NUM_THREADS=1 python avg_model_ind.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt linear none ../results &
 OMP_NUM_THREADS=1 python avg_model_ind.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt mlp none ../results &
 
+OMP_NUM_THREADS=1 python avg_model_ind.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt rbf none ../results --bias &
+OMP_NUM_THREADS=1 python avg_model_ind.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt mat32 none ../results --bias &
+OMP_NUM_THREADS=1 python avg_model_ind.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt mat52 none ../results --bias &
+OMP_NUM_THREADS=1 python avg_model_ind.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt ratquad none ../results --bias &
+OMP_NUM_THREADS=1 python avg_model_ind.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt linear none ../results --bias &
+OMP_NUM_THREADS=1 python avg_model_ind.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt mlp none ../results --bias &
+
 #OMP_NUM_THREADS=1 python avg_model_ind.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt ridge none ../results --ard &
 #OMP_NUM_THREADS=1 python avg_model_ind.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt svr none ../results --ard &
 #OMP_NUM_THREADS=1 python avg_model_ind.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt rbf none ../results --ard &
