3c6411b768e70136ab18971ee4de77d8e3ceebdf
Daniel Beck <daniel.e.beck@gmail.com>
2017-01-03 19:35:02 +1100

    new icm

--- emotions/src/avg_model_icm.py
+++ emotions/src/avg_model_icm.py
@@ -38,6 +38,7 @@ parser.add_argument('--rank', help='rank of coreg matrix', default=1, type=int)
 parser.add_argument('--data_size', help='size of dataset, default is full size',
                     default=10000, type=int)
 parser.add_argument('--ard', help='set this flag to enable ARD', action='store_true')
+parser.add_argument('--bias', help='set this flag to add a bias kernel', action='store_true')
 args = parser.parse_args()
 
 ###########
@@ -73,10 +74,13 @@ folds = kf.split(data)
 
 ##############
 # Create output structure
-if args.ard:
-    mode = 'ard'
-else:
-    mode = 'iso'
+#if args.ard:
+#    mode = 'ard'
+#else:
+#    mode = 'iso'
+mode = 'iso'
+if args.bias:
+    mode += '_bias'
 main_out_dir = os.path.join(args.output_dir, 'avg', 'icm', args.model + '_' + mode, args.label_preproc)
 
 #############
@@ -101,16 +105,16 @@ for i_train, i_test in folds:
         Y_test_list.append(Y_test[:, emo_id:emo_id+1])
 
     # Scale Y if asked for
-    if args.label_preproc == "scale":
-        Y_scaler_list = []
-        for emo_id, emo in enumerate(EMOS):
-            Y_scaler = pp.StandardScaler()
-            Y_scaler.fit(Y_train_list[emo_id])
-            Y_train_list[emo_id] = Y_scaler.transform(Y_train_list[emo_id])
-            Y_scaler_list.append(Y_scaler)
-    elif args.label_preproc == "warp":
-        # Warped GPs seems to break if we have too many zeroes.
-        Y_train_list = [Y_train - 50 for Y_train in Y_train_list]
+    # if args.label_preproc == "scale":
+    #     Y_scaler_list = []
+    #     for emo_id, emo in enumerate(EMOS):
+    #         Y_scaler = pp.StandardScaler()
+    #         Y_scaler.fit(Y_train_list[emo_id])
+    #         Y_train_list[emo_id] = Y_scaler.transform(Y_train_list[emo_id])
+    #         Y_scaler_list.append(Y_scaler)
+    # elif args.label_preproc == "warp":
+    #     # Warped GPs seems to break if we have too many zeroes.
+    #     Y_train_list = [Y_train - 50 for Y_train in Y_train_list]
     
     # Select and train model
     # TODO: implement ridge and svr using EasyAdapt
@@ -138,16 +142,19 @@ for i_train, i_test in folds:
             k = GPy.kern.Linear(X.shape[1], ARD=args.ard)
         elif args.model == 'mlp':
             k = GPy.kern.MLP(X.shape[1], ARD=args.ard)
-        if args.label_preproc == "warp":
-            model = GPy.models.WarpedGP(X_train, Y_train, kernel=k)
-            model['warp_tanh.psi'] = np.random.lognormal(0, 1, (3, 3))
-        else:
+
+        if args.bias:
+            k = k + GPy.kern.Bias(X.shape[1])
+        #if args.label_preproc == "warp":
+        #    model = GPy.models.WarpedGP(X_train, Y_train, kernel=k)
+        #    model['warp_tanh.psi'] = np.random.lognormal(0, 1, (3, 3))
+        #else:
             #model = GPy.models.GPRegression(X_train, Y_train, kernel=k)
-            icmk = GPy.util.multioutput.ICM(input_dim=X.shape[1], num_outputs=6, 
-                                            kernel=k, W_rank=args.rank)
-            model = GPy.models.GPCoregionalizedRegression(X_train_list,
-                                                          Y_train_list,
-                                                          kernel=icmk)
+        icmk = GPy.util.multioutput.ICM(input_dim=X.shape[1], num_outputs=6, 
+                                        kernel=k, W_rank=args.rank)
+        model = GPy.models.GPCoregionalizedRegression(X_train_list,
+                                                      Y_train_list,
+                                                      kernel=icmk)
         model.optimize(messages=True, max_iters=100)
         print model
     
@@ -174,17 +181,18 @@ for i_train, i_test in folds:
                 # TODO: preprocessing
                 emo_dict = {}
                 to_predict = np.concatenate((X_test_list[emo_id], np.ones((X_test.shape[0], 1)) * emo_id), axis=1)
-                preds, vars = model.predict_noiseless(to_predict)
-                if args.label_preproc == 'scale':
-                    preds = Y_scaler_list[emo_id].inverse_transform(preds)
+                noise_dict = {'output_index': np.ones((X_test.shape[0], 1), dtype=int) * (emo_id)}
+                preds, vars = model.predict(to_predict, Y_metadata=noise_dict)
+                #if args.label_preproc == 'scale':
+                #    preds = Y_scaler_list[emo_id].inverse_transform(preds)
                 emo_dict['mae'] = MAE(preds, Y_test_list[emo_id])
                 emo_dict['rmse'] = np.sqrt(MSE(preds, Y_test_list[emo_id]))
                 emo_dict['pearsonr'] = pearsonr(preds.flatten(), Y_test_list[emo_id].flatten())
-                Y_metadata = {}
-                Y_metadata['output_index'] = np.ones(X_test.shape[0]) * emo_id
+                #Y_metadata = {}
+                #Y_metadata['output_index'] = np.ones(X_test.shape[0]) * emo_id
                 emo_dict['nlpd'] = -np.mean(model.log_predictive_density(to_predict, 
                                                                          Y_test_list[emo_id],
-                                                                         Y_metadata=Y_metadata))
+                                                                         Y_metadata=noise_dict))
                 info_dict[emo] = emo_dict
                 preds_list.append(preds.flatten())
                 vars_list.append(vars.flatten())
@@ -198,45 +206,56 @@ for i_train, i_test in folds:
             info_dict['regularization'] = model.best_params_['C']
             info_dict['epsilon'] = model.best_params_['epsilon']
             info_dict['gamma'] = model.best_params_['gamma']
-        elif args.model == 'rbf':
-            info_dict['variance'] = float(model['ICM.rbf.variance'])
-            info_dict['lengthscale'] = list(model['ICM.rbf.lengthscale'])
-            info_dict['noise'] = list([float(noise) for noise in model['mixed_noise.*']])
+        else:
+            param_names = model.parameter_names()
+            for p_name in param_names:
+                if p_name == 'ICM.B.W':
+                    info_dict[p_name] = list([list(pars) for pars in model[p_name]])
+                else:
+                    try:
+                        info_dict[p_name] = float(model[p_name])
+                    except TypeError: #ARD
+                        info_dict[p_name] = list(model[p_name])
             info_dict['log_likelihood'] = float(model.log_likelihood())
-        elif args.model == 'mat32':
-            info_dict['variance'] = float(model['ICM.Mat32.variance'])
-            info_dict['lengthscale'] = list(model['ICM.Mat32.lengthscale'])
-            info_dict['noise'] = list([float(noise) for noise in model['mixed_noise.*']])
-            info_dict['log_likelihood'] = float(model.log_likelihood())
-        elif args.model == 'mat52':
-            info_dict['variance'] = float(model['ICM.Mat52.variance'])
-            info_dict['lengthscale'] = list(model['ICM.Mat52.lengthscale'])
-            info_dict['noise'] = list([float(noise) for noise in model['mixed_noise.*']])
-            info_dict['log_likelihood'] = float(model.log_likelihood())
-        elif args.model == 'ratquad':
-            info_dict['variance'] = float(model['ICM.RatQuad.variance'])
-            info_dict['lengthscale'] = list(model['ICM.RatQuad.lengthscale'])
-            info_dict['power'] = list(model['ICM.RatQuad.power'])
-            info_dict['noise'] = list([float(noise) for noise in model['mixed_noise.*']])
-            info_dict['log_likelihood'] = float(model.log_likelihood())
-        elif args.model == 'linear':
-            info_dict['variance'] = list(model['ICM.linear.variances'])
-            info_dict['noise'] = list([float(noise) for noise in model['mixed_noise.*']])
-            info_dict['log_likelihood'] = float(model.log_likelihood())
-        elif args.model == 'mlp':
-            info_dict['variance'] = float(model['ICM.mlp.variance'])
-            info_dict['weight_variance'] = list(model['ICM.mlp.weight_variance'])
-            info_dict['bias_variance'] = float(model['ICM.mlp.bias_variance'])
-            info_dict['noise'] = list([float(noise) for noise in model['mixed_noise.*']])
-            info_dict['log_likelihood'] = float(model.log_likelihood())
+        # elif args.model == 'rbf':
+        #     info_dict['variance'] = float(model['ICM.rbf.variance'])
+        #     info_dict['lengthscale'] = list(model['ICM.rbf.lengthscale'])
+        #     info_dict['noise'] = list([float(noise) for noise in model['mixed_noise.*']])
+        #     info_dict['log_likelihood'] = float(model.log_likelihood())
+        # elif args.model == 'mat32':
+        #     info_dict['variance'] = float(model['ICM.Mat32.variance'])
+        #     info_dict['lengthscale'] = list(model['ICM.Mat32.lengthscale'])
+        #     info_dict['noise'] = list([float(noise) for noise in model['mixed_noise.*']])
+        #     info_dict['log_likelihood'] = float(model.log_likelihood())
+        # elif args.model == 'mat52':
+        #     info_dict['variance'] = float(model['ICM.Mat52.variance'])
+        #     info_dict['lengthscale'] = list(model['ICM.Mat52.lengthscale'])
+        #     info_dict['noise'] = list([float(noise) for noise in model['mixed_noise.*']])
+        #     info_dict['log_likelihood'] = float(model.log_likelihood())
+        # elif args.model == 'ratquad':
+        #     info_dict['variance'] = float(model['ICM.RatQuad.variance'])
+        #     info_dict['lengthscale'] = list(model['ICM.RatQuad.lengthscale'])
+        #     info_dict['power'] = list(model['ICM.RatQuad.power'])
+        #     info_dict['noise'] = list([float(noise) for noise in model['mixed_noise.*']])
+        #     info_dict['log_likelihood'] = float(model.log_likelihood())
+        # elif args.model == 'linear':
+        #     info_dict['variance'] = list(model['ICM.linear.variances'])
+        #     info_dict['noise'] = list([float(noise) for noise in model['mixed_noise.*']])
+        #     info_dict['log_likelihood'] = float(model.log_likelihood())
+        # elif args.model == 'mlp':
+        #     info_dict['variance'] = float(model['ICM.mlp.variance'])
+        #     info_dict['weight_variance'] = list(model['ICM.mlp.weight_variance'])
+        #     info_dict['bias_variance'] = float(model['ICM.mlp.bias_variance'])
+        #     info_dict['noise'] = list([float(noise) for noise in model['mixed_noise.*']])
+        #     info_dict['log_likelihood'] = float(model.log_likelihood())
 
-        if args.model != 'ridge' and args.model != 'svr':
-            info_dict['W'] = list([list(w) for w in model['ICM.B.W']])
-            info_dict['kappa'] = list([float(kappa) for kappa in model['ICM.B.kappa']])
+        #if args.model != 'ridge' and args.model != 'svr':
+        #    info_dict['W'] = list([list(w) for w in model['ICM.B.W']])
+        #    info_dict['kappa'] = list([float(kappa) for kappa in model['ICM.B.kappa']])
 
-        if args.label_preproc == 'warp':
-            info_dict['warp_psi'] = list([list(pars) for pars in model['warp_tanh.psi']])
-            info_dict['warp_d'] = float(model['warp_tanh.d'])
+        #if args.label_preproc == 'warp':
+        #    info_dict['warp_psi'] = list([list(pars) for pars in model['warp_tanh.psi']])
+        #    info_dict['warp_d'] = float(model['warp_tanh.d'])
         
     
         # Save information
---
+++ emotions/src/run_avg_icm.sh
@@ -0,0 +1,28 @@
+OMP_NUM_THREADS=1 nohup python avg_model_icm.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt mat32 none ../results/ --rank 1 > icm_nohups/mat32_1.nohup &
+OMP_NUM_THREADS=1 nohup python avg_model_icm.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt mat52 none ../results/ --rank 1 > icm_nohups/mat52_1.nohup &
+OMP_NUM_THREADS=1 nohup python avg_model_icm.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt mat32 none ../results/ --rank 1 --bias > icm_nohups/mat32_1_bias.nohup &
+OMP_NUM_THREADS=1 nohup python avg_model_icm.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt mat52 none ../results/ --rank 1 --bias > icm_nohups/mat52_1_bias.nohup &
+
+OMP_NUM_THREADS=1 nohup python avg_model_icm.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt mat32 none ../results/ --rank 2 > icm_nohups/mat32_2.nohup &
+OMP_NUM_THREADS=1 nohup python avg_model_icm.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt mat52 none ../results/ --rank 2 > icm_nohups/mat52_2.nohup &
+OMP_NUM_THREADS=1 nohup python avg_model_icm.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt mat32 none ../results/ --rank 2 --bias > icm_nohups/mat32_2_bias.nohup &
+OMP_NUM_THREADS=1 nohup python avg_model_icm.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt mat52 none ../results/ --rank 2 --bias > icm_nohups/mat52_2_bias.nohup &
+
+OMP_NUM_THREADS=1 nohup python avg_model_icm.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt mat32 none ../results/ --rank 3 > icm_nohups/mat32_3.nohup &
+OMP_NUM_THREADS=1 nohup python avg_model_icm.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt mat52 none ../results/ --rank 3 > icm_nohups/mat52_3.nohup &
+OMP_NUM_THREADS=1 nohup python avg_model_icm.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt mat32 none ../results/ --rank 3 --bias > icm_nohups/mat32_3_bias.nohup &
+OMP_NUM_THREADS=1 nohup python avg_model_icm.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt mat52 none ../results/ --rank 3 --bias > icm_nohups/mat52_3_bias.nohup &
+
+OMP_NUM_THREADS=1 nohup python avg_model_icm.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt mat32 none ../results/ --rank 4 > icm_nohups/mat32_4.nohup &
+OMP_NUM_THREADS=1 nohup python avg_model_icm.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt mat52 none ../results/ --rank 4 > icm_nohups/mat52_4.nohup &
+OMP_NUM_THREADS=1 nohup python avg_model_icm.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt mat32 none ../results/ --rank 4 --bias > icm_nohups/mat32_4_bias.nohup &
+OMP_NUM_THREADS=1 nohup python avg_model_icm.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt mat52 none ../results/ --rank 4 --bias > icm_nohups/mat52_4_bias.nohup &
+
+OMP_NUM_THREADS=1 nohup python avg_model_icm.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt mat32 none ../results/ --rank 5 > icm_nohups/mat32_5.nohup &
+OMP_NUM_THREADS=1 nohup python avg_model_icm.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt mat52 none ../results/ --rank 5 > icm_nohups/mat52_5.nohup &
+OMP_NUM_THREADS=1 nohup python avg_model_icm.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt mat32 none ../results/ --rank 5 --bias > icm_nohups/mat32_5_bias.nohup &
+OMP_NUM_THREADS=1 nohup python avg_model_icm.py ../../data/instances.txt ../../data/emotions.gold ../../embs/glove.6B.100d.txt mat52 none ../results/ --rank 5 --bias > icm_nohups/mat52_5_bias.nohup &
+
+
+
+
